d5d92dd8dd246fde609a5a7a42939b3329d65ca0
Ben <thisbenrogers@gmail.com>
2019-11-11 15:44:51 -0600

    :sparkles: Adds /users endpoint

--- API/api-router.js
+++ API/api-router.js
@@ -1,13 +1,55 @@
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
-const authRouter = require('../users/auth-router.js');
-// const usersRouter = require('../users/users-router.js');
+const requiresAuth = require('../middleware/restricted-middleware');
 
-router.use('/auth', authRouter);
-// router.use('/users', usersRouter);
+const Users = require('../users/users-model');
 
 router.get('/', (req, res) => {
   res.send("<h1>🚀</h1>");
 });
+
+
+router.post('/register', (req, res) => {
+  let userInfo = req.body;
+
+  bcrypt.hash(userInfo.password, 12, (err, hashed) => {
+    userInfo.password = hashed;
+
+    Users.add(userInfo)
+      .then(saved => {
+        res.status(200).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `${user.username} is logged in` });
+      } else {
+        res.status(401).json({ mesage: "You shall not pass!"});
+      }
+    })
+    .catch(error => {
+      console.log('login error', error);
+      res.status(500).json(error);
+    })
+})
+
+router.get('/users', requiresAuth, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+})
 
 module.exports = router;
